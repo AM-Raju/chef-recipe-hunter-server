2d827e8b752f4f01c573fdc766b12ed7a00cd0b4
AM-Raju <mdmorshed.sj2005@gmail.com>
2023-05-02 17:44:08 +0600

    cors intalled and ran server for chef data

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 5000;
 
+app.use(cors());
+const chefs = require("./data/chefData.json");
+
 app.get("/", (req, res) => {
   res.send("Server is running");
+});
+
+// Get all chefs data
+app.get("/chefs", (req, res) => {
+  res.send(chefs);
 });
 
 app.listen(port, () => {
