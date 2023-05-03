1219eb918042a60fe34395dd26a290d34c433e6e
AM-Raju <mdmorshed.sj2005@gmail.com>
2023-05-03 16:56:48 +0600

    Sever side work completed

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = 5000;
 
 app.use(cors());
 const chefs = require("./data/chefData.json");
+const recipes = require("./data/reciepes.json");
 
 app.get("/", (req, res) => {
   res.send("Server is running");
@@ -13,6 +14,16 @@ app.get("/", (req, res) => {
 // Get all chefs data
 app.get("/chefs", (req, res) => {
   res.send(chefs);
+});
+
+app.get("/chefs/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedChef = chefs.find((chef) => chef.id === id);
+  res.send(selectedChef);
+});
+
+app.get("/recipes", (req, res) => {
+  res.send(recipes);
 });
 
 app.listen(port, () => {
